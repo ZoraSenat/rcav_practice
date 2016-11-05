09d98cdcf2281ed24f8ccc5fef8ad9379245b04b
zora <ssh>
2016-11-05 09:59:48 -0500

    Created Routes, Controllers, Actions and View for Square and Square Root

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,25 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @number = params[:number]
+    @number_squared = (@number.to_i * @number.to_i)
+
+  end
+
+  def square_root
+    @number = params[:number]
+    @number_root = @number.to_i.
+  end
+
+  def random
+
+  end
+
+  def payment
+
+  end
+
+
 end
