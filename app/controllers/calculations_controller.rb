ddf4a66b2b1d578720be162e2e84922ef7bd03a9
zora <ssh>
2016-11-05 10:16:48 -0500

    Added calculations for Payment and Random Number to controller and updated views

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,4 +1,5 @@
 class CalculationsController < ApplicationController
+
   def instructions
     render("instructions.html.erb")
   end
@@ -6,20 +7,28 @@ class CalculationsController < ApplicationController
   def square
     @number = params[:number]
     @number_squared = (@number.to_i * @number.to_i)
-
   end
 
   def square_root
     @number = params[:number]
-    @number_root = @number.to_i.
+    @number_root = Math.sqrt(@number.to_i)
   end
 
   def random
-
+    @min = params[:min]
+    @max = params[:max]
+    @random = rand(@min.to_i..@max.to_i)
   end
 
   def payment
-
+    #Parameters: {"interest_rate"=>"34", "years"=>"60", "principal"=>"30000"}
+    @apr = params[:interest_rate].to_f/100
+    @years = params[:years].to_f
+    @principal = params[:principal].to_f
+    @periods = @years*12
+    @rate = @apr/12
+ #fix math
+    @payment = (@rate * @principal*((1+@rate)**@periods))/((1+@rate)**@periods-1).to_i
   end
 
 
